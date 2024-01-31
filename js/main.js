96c38f25dbfef9d4e438c3260f7982b69df076c6
frondendasoslar <faxriddin19951021@gmai.com>
2024-01-31 17:49:42 +0500

    initial commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+//JavaScripda matinlar har doim qo'shtirnoq ichiga yozilishi kerak.
+const phoneModel = 'Xiomi POCO X3';
+// const - o'zgaruvchi;
+//phoneModel - Xiomi POCO X3 matni bog'langan nom
+const phoneColor = 'Black';
+const phoneSize = '128GB'
+const phoneCamera = '64MP';
+const lastname = 'Mamadiyev';
+const neyme = 'Faxriddin';
+const year = '29';
+
+
+console.log(phoneModel);
+//Consolda phoneModel nomini ya'ni Xiomi POCO X3 matnini chiqaradi
+console.log(phoneColor);
+console.log(phoneSize);
+console.log(phoneCamera);
+console.log(lastname);
+console.log(neyme);
+console.log(year);
